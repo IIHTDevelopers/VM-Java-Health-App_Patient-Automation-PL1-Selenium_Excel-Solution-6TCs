521331007679f122f65b74b1125ba817e6632a33
IIHT Developers <93914497+IIHTDevelopers@users.noreply.github.com>
2025-05-05 13:24:38 +0530

    Add files via upload

---
+++ src/main/java/coreUtilities/testutils/TestResults.java
@@ -0,0 +1,40 @@
+package coreUtilities.testutils;
+
+public class TestResults {
+
+	private String testCaseResults;
+	private String customData;
+	private String hostName;
+	private String attemptId;
+
+	public String getTestCaseResults() {
+		return testCaseResults;
+	}
+
+	public void setTestCaseResults(String testCaseResults) {
+		this.testCaseResults = testCaseResults;
+	}
+
+	public String getCustomData() {
+		return customData;
+	}
+
+	public void setCustomData(String customData) {
+		this.customData = customData;
+	}
+	public String getHostName() {
+		return hostName;
+	}
+
+	public void setHosttName(String hostName) {
+		this.hostName = hostName;
+	}
+	public String getAttemptId() {
+		return attemptId;
+	}
+
+	public void setAttemptId(String attemptId) {
+		this.attemptId = attemptId;
+	}
+
+}
